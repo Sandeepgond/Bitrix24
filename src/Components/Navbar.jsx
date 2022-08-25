43a8375a52159da80f8df593352550127a425e24
Sandeepgond <sandeepgond13sandy@gmail.com>
2022-08-24 20:54:21 -0700

    day2

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+
+import "./Navbar.css"
+export function  Navbar () {
+    return <>
+    <div id="container">
+        <div > <img class="image1" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwGQTlNdYPjsCIMqY3h2GwbJWofSfcFFz7rL5ET5joQowopQ7e0DF4R47SA4AZwVVxkro&usqp=CAU" alt=""/></div>
+        <div id="category">
+            <div>TOOLS</div>
+            <div>PRICING</div>
+            <div>SOLUTIONS</div>
+            <div>PARTNERS</div>
+            <div>APP</div>
+            <div>BLOG</div>
+            <div>SUPPORT</div> 
+        </div>
+        <div id="navend">
+        <div> <button class="signup">START FOR FREE</button></div>
+            <div>LOGIN</div>
+            <div>EN</div>
+           
+        </div>
+    </div>
+    
+    
+    
+    
+    </>
+}
---
+++ src/Components/SignUpPage.jsx
@@ -0,0 +1,7 @@
+export function SignUp(){
+    return (
+        <div>
+
+        </div>
+    )
+}
