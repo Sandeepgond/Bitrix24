ce44826fef1815ec336b7380f7ba65b6811554fb
Sandeepgond <sandeepgond13sandy@gmail.com>
2022-08-25 19:41:41 -0700

    day3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
 import './App.css';
 import { Home } from './Components/HomePage';
-
+import 'font-awesome/css/font-awesome.min.css'
 function App() {
+
   return (
+    
     <div className="App">
       <Home/>
     </div>
